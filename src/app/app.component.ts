bef20ae5d3958874a16df8944884af05679329c3
Yagudagu <Jacobhmartian@gmail.com>
2021-10-14 18:31:42 +0000

    please?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<a href="/login">Login with more words, to know a change</a>`,
+  template: `<a href="/login">Login, does it work?</a>`,
 })
 export class AppComponent {
   value = 'World';
