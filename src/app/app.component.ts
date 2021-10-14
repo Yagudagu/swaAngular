9bd65e89088e9d50046f0a538b564b04c3d0ca0a
Yagudagu <Jacobhmartian@gmail.com>
2021-10-14 16:51:23 +0000

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<a href="/login">Login with more words</a>`,
+  template: `<a href="/login">Login with more words, to know a change</a>`,
 })
 export class AppComponent {
   value = 'World';
