f36d61ca933fb1a3f1009abb076e473730930e47
Yagudagu <Jacobhmartian@gmail.com>
2021-10-18 20:08:50 +0000

    added logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<a href="/login">Login, does it work?</a>`,
+  template: `<a href="/login">Login, does it work?</a
+    ><a href="/logout">Logout, let's try it</a>`,
 })
 export class AppComponent {
   value = 'World';
